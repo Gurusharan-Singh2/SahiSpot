36be20020c9c0a82636a7bfef6781df758621499
manish-yadav4 <manishy8572@gmail.com>
2025-09-11 18:09:30 +0530

    Hover effect

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -10,11 +10,11 @@ const Hero = () => {
         placeholder="Enter your location"
         className="border px-4 py-2 rounded w-full mb-4"
       />
-      <div>
-        <button className="bg-blue-600 text-white px-6 py-2 rounded-full mr-2">
+      <div className="flex gap-10 justify-center">
+        <button className="bg-Primary text-white px-6 py-2 rounded-full mr-2 text-black hover:bg-white hover:border-2 cursor-pointer hover:border-Primary hover:scale-110 transition-all duration-300 hover:text-black">
           Find Parking
         </button>
-        <button className="bg-gray-200 px-6 py-2 rounded-full">
+        <button className=" border-2 border-Primary px-6 py-2 rounded-full text-black font-semibold hover:bg-Primary cursor-pointer hover:text-white hover:scale-110 transition-all duration-300">
           Reserve a Spot
         </button>
       </div>
